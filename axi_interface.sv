8b3b9ed4aa9776ea2149c18682228f1b08c7834b
Rishav Kumar <kumarrishav14@gmail.com>
2021-01-10 13:21:29 +0530

    Added assertions to interface

--- axi_interface.sv
+++ axi_interface.sv
@@ -32,6 +32,10 @@ interface axi_intf #(parameter A_WIDTH = 16, D_WIDTH = 16)(input bit clk, bit rs
     logic [1:0] RRESP;
     logic RLAST, RVALID, RREADY;
 
+    /* Clocking Blocks: 3 CBs are defined as follows
+            1. m_drv_cb - Clocking block for master driver
+            2. s_drv_cb - Clocking block for slave driver
+            3. mon_cb   - Clocking block for monitors of both master and slave */
     clocking m_drv_cb @(posedge clk);
         output AWID, AWADDR, AWLEN, AWSIZE, AWBURST,AWVALID, WID, WDATA, WSTRB, WLAST, WVALID, 
                 BREADY, ARID, ARADDR, ARLEN, ARSIZE, ARBURST, ARVALID, RREADY;
@@ -51,7 +55,56 @@ interface axi_intf #(parameter A_WIDTH = 16, D_WIDTH = 16)(input bit clk, bit rs
     endclocking
 
     modport MDRV(clocking m_drv_cb, input rstn);
-    modport MMON(clocking mon_cb, input rstn);
+    modport MMON(clocking mon_cb,   input rstn);
     modport SDRV(clocking s_drv_cb, input rstn);
-    modport SMON(clocking mon_cb, input rstn);
+    modport SMON(clocking mon_cb,   input rstn);
+
+    // *************************************************************************************************
+    //                                      Assertions
+    // *************************************************************************************************
+    // Property to check whether all write address channel remains stable after AWVALID is asserted
+    property aw_valid;
+        @(posedge clk) $rose(AWVALID) |-> ( $stable(AWID)   
+                                            &&$stable(AWADDR)
+                                            &&$stable(AWLEN)
+                                            &&$stable(AWSIZE) 
+                                            &&$stable(AWBURST)) throughout AWREADY[->1];
+    endproperty
+
+    // Property to check whether all write address channel remains stable after AWVALID is asserted
+    property w_valid;
+        @(posedge clk) $rose(WVALID) |-> (  $stable(WID) 
+                                            && $stable(WDATA)
+                                            && $stable(WSTRB)
+                                            && $stable(WLAST)) throughout WREADY[->1];
+    endproperty
+
+    // Property to check whether all write address channel remains stable after AWVALID is asserted
+    property b_valid;
+        @(posedge clk) $rose(BVALID) |-> (  $stable(BID) 
+                                            && $stable(BRESP)) throughout BREADY[->1];
+    endproperty
+
+    // Property to check whether all write address channel remains stable after AWVALID is asserted
+    property ar_valid;
+        @(posedge clk) $rose(ARVALID) |-> ( $stable(ARID)   
+                                            &&$stable(ARADDR)
+                                            &&$stable(ARLEN)
+                                            &&$stable(ARSIZE) 
+                                            &&$stable(ARBURST)) throughout ARREADY[->1];
+    endproperty
+
+    // Property to check whether all write address channel remains stable after AWVALID is asserted
+    property r_valid;
+        @(posedge clk) $rose(RVALID) |-> (  $stable(RID) 
+                                            && $stable(RDATA)
+                                            && $stable(RRESP)
+                                            && $stable(RLAST)) throughout RREADY[->1];
+    endproperty
+
+    assert property (aw_valid);
+    assert property (w_valid);
+    assert property (b_valid);
+    assert property (ar_valid);
+    assert property (r_valid);
 endinterface //axi_intf
